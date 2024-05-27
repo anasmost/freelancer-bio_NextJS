5f1f5c21611a058f0922014bac577507633a2ac2
Anas Mostafa <anas20chakir@outlook.com>
2024-05-27 14:32:56 +0100

    Change Signature's font family and weight

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,18 +6,18 @@ export default function Footer() {
   return (
     <footer className="backdrop-blur-lg bg-opacity-25 shadow-small dark:shadow-inner shadow-[var(--shadow-color)] dark:shadow-[var(--shadow-color)]">
       <p
-        className={`${handwriting.className} text-center tracking-wider text-3xl leading-10 text-[var(--foreground-color)]`}
+        className={`${handwriting.className} text-center tracking-wide text-2xl font-bold text-[var(--foreground-color)]`}
       >
-        Copyright &nbsp;
+        Copyright{" "}
         <Avatar
           src={img.src}
           alt="Anas Avatar"
           size="md"
           radius="lg"
           isBordered
-          className="bg-gradient-radial from-red-100 dark:from-red-50 outline-none inline-block ring-[var(--foreground-color)] ring-offset-transparent ring-offset-0 align-text-top"
+          className="bg-gradient-radial from-red-100 dark:from-red-50 outline-none inline-block ring-[var(--foreground-color)] ring-offset-transparent ring-offset-1 align-text-top"
         />
-        <span className="align-super text-4xl leading-[0]">&copy;</span> {new Date().getFullYear()}
+        <span className="align-super text-3xl">&copy;</span> {new Date().getFullYear()}
       </p>
     </footer>
   );
